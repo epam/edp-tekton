2d2e6877dabec404b4e17bd6f95f85516a97c6ba
Artem Zahumonnyi <Artem_Zahumonnyi@epam.com>
2022-10-18 09:29:37 +0300

    [EPMDEDP-10736]: chore: Added support for using branches with a slash (Buildtool Gradle)
    * update-build-number-maven Task was implemented
    * update-cbis Task: User should use codebase name with only letters and dashes for CBIS_NAME parameter
    * get-version-edp Task: Codebasebranches custom resource cannot contain characters other than letters and dashes in the name
    * get-nexus-repository-url Task: If the User pushes the release application to Nexus then the User uses edp-maven-releases Nexus repository, else edp-maven-snapshots Nexus repository is used
    * sonar-prepare-files-gradle and sonar-prepare-files-maven Tasks: The User uses branch name without '/' symbols and lowercase for the Sonar project key
    
    Change-Id: I6d521650bfda19b4219855bf2f530b5345dcbf11

---
+++ charts/pipelines-library/tests/test_java_maven_pipelines.py
@@ -0,0 +1,132 @@
+import os
+import sys
+
+from .helpers import helm_template
+
+
+def test_java_pipelines_gerrit():
+    config = """
+gerrit:
+  enabled: true
+    """
+
+    r = helm_template(config)
+
+    # ensure pipelines have proper steps
+    for buildtool in ['maven']:
+        for framework in ['java8', 'java11']:
+            for cbtype in ['app', 'lib']:
+
+                assert f"gerrit-{buildtool}-{framework}-{cbtype}-review" in r["pipeline"]
+                assert f"gerrit-{buildtool}-{framework}-{cbtype}-build-default" in r["pipeline"]
+                assert f"gerrit-{buildtool}-{framework}-{cbtype}-build-edp" in r["pipeline"]
+
+                gerrit_review_pipeline = f"gerrit-{buildtool}-{framework}-{cbtype}-review"
+                gerrit_build_pipeline_def = f"gerrit-{buildtool}-{framework}-{cbtype}-build-default"
+                gerrit_build_pipeline_edp = f"gerrit-{buildtool}-{framework}-{cbtype}-build-edp"
+
+                rt = r["pipeline"][gerrit_review_pipeline]["spec"]["tasks"]
+                assert "fetch-repository" in rt[0]["name"]
+                assert "gerrit-notify" in rt[1]["name"]
+                assert "init-values" in rt[2]["name"]
+                assert "compile" in rt[3]["name"]
+                assert "test" in rt[4]["name"]
+                assert "fetch-target-branch" in rt[5]["name"]
+                assert "sonar-prepare-files" in rt[6]["name"]
+                assert f"sonar-prepare-files-{buildtool}" == rt[6]["taskRef"]["name"]
+                assert "sonar" in rt[7]["name"]
+                if cbtype == "app":
+                    if buildtool == "gradle":
+                        assert "build" in rt[8]["name"]
+                        assert "dockerfile-lint" in rt[9]["name"]
+                        assert "dockerbuild-verify" in rt[10]["name"]
+                        assert "helm-lint" in rt[11]["name"]
+                    else:
+                        assert "dockerfile-lint" in rt[8]["name"]
+                        assert "dockerbuild-verify" in rt[9]["name"]
+                        assert "helm-lint" in rt[10]["name"]
+
+                assert "gerrit-vote-success" in r["pipeline"][gerrit_review_pipeline]["spec"]["finally"][0]["name"]
+                assert "gerrit-vote-failure" in r["pipeline"][gerrit_review_pipeline]["spec"]["finally"][1]["name"]
+
+                # build with default versioning
+                btd = r["pipeline"][gerrit_build_pipeline_def]["spec"]["tasks"]
+                assert "fetch-repository" in btd[0]["name"]
+                assert "gerrit-notify" in btd[1]["name"]
+                assert "init-values" in btd[2]["name"]
+                assert "get-version" in btd[3]["name"]
+                # ensure we have default versioning
+                assert f"get-version-{buildtool}-default" == btd[3]["taskRef"]["name"]
+                assert "sonar-cleanup" in btd[4]["name"]
+                if cbtype == "app":
+                    assert "sast" in btd[5]["name"]
+                    assert "compile" in btd[6]["name"]
+                    assert buildtool == btd[6]["taskRef"]["name"]
+                    assert "test" in btd[7]["name"]
+                    assert buildtool == btd[7]["taskRef"]["name"]
+                    assert "sonar" in btd[8]["name"]
+                    assert buildtool == btd[8]["taskRef"]["name"]
+                    assert "build" in btd[9]["name"]
+                    assert buildtool == btd[9]["taskRef"]["name"]
+                    assert "push" in btd[10]["name"]
+                    assert buildtool == btd[10]["taskRef"]["name"]
+                    assert "create-ecr-repository" in btd[11]["name"]
+                    assert "kaniko-build" in btd[12]["name"]
+                    assert "git-tag" in btd[13]["name"]
+                    assert "update-cbis" in btd[14]["name"]
+                else:
+                    assert "compile" in btd[5]["name"]
+                    assert buildtool == btd[5]["taskRef"]["name"]
+                    assert "test" in btd[6]["name"]
+                    assert buildtool == btd[6]["taskRef"]["name"]
+                    assert "sonar" in btd[7]["name"]
+                    assert buildtool == btd[7]["taskRef"]["name"]
+                    assert "build" in btd[8]["name"]
+                    assert buildtool == btd[8]["taskRef"]["name"]
+                    assert "push" in btd[9]["name"]
+                    assert buildtool == btd[9]["taskRef"]["name"]
+                    assert "git-tag" in btd[10]["name"]
+
+                # build with edp versioning
+                btedp = r["pipeline"][gerrit_build_pipeline_edp]["spec"]["tasks"]
+                assert "fetch-repository" in btedp[0]["name"]
+                assert "gerrit-notify" in btedp[1]["name"]
+                assert "init-values" in btedp[2]["name"]
+                assert "get-version" in btedp[3]["name"]
+                assert "get-version-edp" == btedp[3]["taskRef"]["name"]
+                idx = 3
+                # we have update-build-number only for gradle
+                if buildtool == "gradle":
+                    assert "update-build-number" in btedp[4]["taskRef"]["name"]
+                    assert f"update-build-number-{buildtool}" == btedp[4]["taskRef"]["name"]
+                    idx = 4
+
+                assert "sonar-cleanup" in btedp[idx+1]["name"]
+                if cbtype == "app":
+                    assert "sast" in btedp[idx+2]["name"]
+                    assert "compile" in btedp[idx+3]["name"]
+                    assert buildtool == btedp[idx+3]["taskRef"]["name"]
+                    assert "test" in btedp[idx+4]["name"]
+                    assert buildtool == btedp[idx+4]["taskRef"]["name"]
+                    assert "sonar" in btedp[idx+5]["name"]
+                    assert buildtool == btedp[idx+5]["taskRef"]["name"]
+                    assert "build" in btedp[idx+6]["name"]
+                    assert buildtool == btedp[idx+6]["taskRef"]["name"]
+                    assert "push" in btedp[idx+7]["name"]
+                    assert buildtool == btedp[idx+7]["taskRef"]["name"]
+                    assert "create-ecr-repository" in btedp[idx+8]["name"]
+                    assert "kaniko-build" in btedp[idx+9]["name"]
+                    assert "git-tag" in btedp[idx+10]["name"]
+                    assert "update-cbis" in btedp[idx+11]["name"]
+                else:
+                    assert "compile" in btedp[idx+2]["name"]
+                    assert buildtool == btedp[idx+2]["taskRef"]["name"]
+                    assert "test" in btedp[idx+3]["name"]
+                    assert buildtool == btedp[idx+3]["taskRef"]["name"]
+                    assert "sonar" in btedp[idx+4]["name"]
+                    assert buildtool == btedp[idx+4]["taskRef"]["name"]
+                    assert "build" in btedp[idx+5]["name"]
+                    assert buildtool == btedp[idx+5]["taskRef"]["name"]
+                    assert "push" in btedp[idx+6]["name"]
+                    assert buildtool == btedp[idx+6]["taskRef"]["name"]
+                    assert "git-tag" in btedp[idx+7]["name"]

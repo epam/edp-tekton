e1c54c2a211c70d68c1f652ac155e41d0e28136b
Artem Zahumonnyi <Artem_Zahumonnyi@epam.com>
2022-10-19 22:49:58 +0300

    [EPMDEDP-10598]: chore: Add support for Build and Review pipelines of Autotests
    * Build and review pipelines were implemented for Gradle and Maven build tools.
    * Multi-module project type was added to sonar-prepare-files-maven Task.
    * gerrit-listener EventListener got an additional trigger for autotests.
    
    Change-Id: I6b935819460fb683e6e3a540d62f81add75f00fd

---
+++ charts/pipelines-library/tests/test_java_autotest_gradle_pipelines.py
@@ -0,0 +1,57 @@
+import os
+import sys
+
+from .helpers import helm_template
+
+
+def test_java_pipelines_gerrit():
+    config = """
+gerrit:
+  enabled: true
+    """
+
+    r = helm_template(config)
+
+    # ensure pipelines have proper steps
+    for buildtool in ['gradle']:
+        for framework in ['java11', 'java8']:
+            for cbtype in ['aut']:
+
+                assert f"gerrit-{buildtool}-{framework}-{cbtype}-review" in r["pipeline"]
+                assert f"gerrit-{buildtool}-{framework}-{cbtype}-build-default" in r["pipeline"]
+                assert f"gerrit-{buildtool}-{framework}-{cbtype}-build-edp" in r["pipeline"]
+
+                gerrit_review_pipeline = f"gerrit-{buildtool}-{framework}-{cbtype}-review"
+                gerrit_build_pipeline_def = f"gerrit-{buildtool}-{framework}-{cbtype}-build-default"
+                gerrit_build_pipeline_edp = f"gerrit-{buildtool}-{framework}-{cbtype}-build-edp"
+
+                rt = r["pipeline"][gerrit_review_pipeline]["spec"]["tasks"]
+                assert "fetch-repository" in rt[0]["name"]
+                assert "gerrit-notify" in rt[1]["name"]
+                assert "init-values" in rt[2]["name"]
+                assert "fetch-target-branch" in rt[3]["name"]
+                assert "sonar-prepare-files" in rt[4]["name"]
+                assert f"sonar-prepare-files-{buildtool}" == rt[4]["taskRef"]["name"]
+                assert "sonar" in rt[5]["name"]
+                assert "gerrit-vote-success" in r["pipeline"][gerrit_review_pipeline]["spec"]["finally"][0]["name"]
+                assert "gerrit-vote-failure" in r["pipeline"][gerrit_review_pipeline]["spec"]["finally"][1]["name"]
+
+                # build with default versioning
+                btd = r["pipeline"][gerrit_build_pipeline_def]["spec"]["tasks"]
+                assert "fetch-repository" in btd[0]["name"]
+                assert "gerrit-notify" in btd[1]["name"]
+                assert "init-values" in btd[2]["name"]
+                assert "sonar-cleanup" in btd[3]["name"]
+                assert "get-version" in btd[4]["name"]
+                assert f"get-version-{buildtool}-default" == btd[4]["taskRef"]["name"]
+                assert "git-tag" in btd[5]["name"]
+
+                # build with edp versioning
+                btedp = r["pipeline"][gerrit_build_pipeline_edp]["spec"]["tasks"]
+                assert "fetch-repository" in btedp[0]["name"]
+                assert "gerrit-notify" in btedp[1]["name"]
+                assert "init-values" in btedp[2]["name"]
+                assert "sonar-cleanup" in btedp[3]["name"]
+                assert "get-version" in btedp[4]["name"]
+                assert "get-version-edp" == btedp[4]["taskRef"]["name"]
+                assert "git-tag" in btedp[5]["name"]

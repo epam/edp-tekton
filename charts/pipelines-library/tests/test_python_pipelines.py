a0f20bab28609a53a41249fac8b5b4d205c7e520
Artem Zahumonnyi <Artem_Zahumonnyi@epam.com>
2022-10-25 17:39:48 +0300

    [EPMDEDP-10605]: chore: Add Build and Review pipelines of Python for GitHub vcs
    Change-Id: Icd30f77fa62941403b99ef1906f533a3742e6e74

--- charts/pipelines-library/tests/test_python_pipelines.py
+++ charts/pipelines-library/tests/test_python_pipelines.py
@@ -96,3 +96,83 @@ gerrit:
                     assert "update-cbis" in btedp[14]["name"]
                 else:
                     assert "git-tag" in btedp[11]["name"]
+
+def test_python_pipelines_gerrit():
+    config = """
+github:
+  enabled: true
+    """
+
+    r = helm_template(config)
+
+    # ensure pipelines have proper steps
+    for buildtool in ['python']:
+        for framework in ['python-3.8']:
+            for cbtype in ['app']:
+
+                vcs = "github"
+                github_review_pipeline = f"{vcs}-{buildtool}-{framework}-{cbtype}-review"
+                github_build_pipeline_def = f"{vcs}-{buildtool}-{framework}-{cbtype}-build-default"
+                github_build_pipeline_edp = f"{vcs}-{buildtool}-{framework}-{cbtype}-build-edp"
+
+                assert github_review_pipeline in r["pipeline"]
+                assert github_build_pipeline_def in r["pipeline"]
+                assert github_build_pipeline_edp in r["pipeline"]
+
+                rt = r["pipeline"][github_review_pipeline]["spec"]["tasks"]
+                assert "github-set-pending-status" in rt[0]["name"]
+                assert "fetch-repository" in rt[1]["name"]
+                assert "init-values" in rt[2]["name"]
+                assert "compile" in rt[3]["name"]
+                assert "test" in rt[4]["name"]
+                assert "sonar" in rt[5]["name"]
+                assert "dockerfile-lint" in rt[6]["name"]
+                assert "dockerbuild-verify" in rt[7]["name"]
+                assert "helm-lint" in rt[8]["name"]
+
+                assert "github-set-success-status" in r["pipeline"][github_review_pipeline]["spec"]["finally"][0]["name"]
+                assert "github-set-failure-status" in r["pipeline"][github_review_pipeline]["spec"]["finally"][1]["name"]
+
+                # build with default versioning
+                btd = r["pipeline"][github_build_pipeline_def]["spec"]["tasks"]
+                assert "fetch-repository" in btd[0]["name"]
+                assert "init-values" in btd[1]["name"]
+                assert "get-version" in btd[2]["name"]
+                assert f"get-version-{buildtool}-default" == btd[2]["taskRef"]["name"]
+                assert "compile" in btd[3]["name"]
+                assert "test" in btd[4]["name"]
+                assert buildtool == btd[4]["taskRef"]["name"]
+                assert "sonar" in btd[5]["name"]
+                assert "sonarqube-scanner" == btd[5]["taskRef"]["name"]
+                assert "get-nexus-repository-url" in btd[6]["name"]
+                assert "get-nexus-repository-url-python" == btd[6]["taskRef"]["name"]
+                assert "push" in btd[7]["name"]
+                assert buildtool == btd[7]["taskRef"]["name"]
+                assert "create-ecr-repository" in btd[8]["name"]
+                assert "kaniko-build" in btd[9]["name"]
+                assert "git-tag" in btd[10]["name"]
+                assert "update-cbis" in btd[11]["name"]
+
+                # build with edp versioning
+                btedp = r["pipeline"][github_build_pipeline_edp]["spec"]["tasks"]
+                assert "fetch-repository" in btedp[0]["name"]
+                assert "init-values" in btedp[1]["name"]
+                assert "get-version" in btedp[2]["name"]
+                assert "get-version-edp" == btedp[2]["taskRef"]["name"]
+                assert "update-build-number" in btedp[3]["taskRef"]["name"]
+                assert f"update-build-number-{buildtool}" == btedp[3]["taskRef"]["name"]
+                assert "compile" in btedp[4]["name"]
+                assert buildtool == btedp[4]["taskRef"]["name"]
+                assert "test" in btedp[5]["name"]
+                assert buildtool == btedp[5]["taskRef"]["name"]
+                assert "sonar" in btedp[6]["name"]
+                assert "sonarqube-scanner" == btedp[6]["taskRef"]["name"]
+                assert "get-nexus-repository-url" in btedp[7]["name"]
+                assert "get-nexus-repository-url-python" == btedp[7]["taskRef"]["name"]
+                assert "push" in btedp[8]["name"]
+                assert buildtool == btedp[8]["taskRef"]["name"]
+                assert "create-ecr-repository" in btedp[9]["name"]
+                assert "kaniko-build" in btedp[10]["name"]
+                assert "git-tag" in btedp[11]["name"]
+                assert "update-cbis" in btedp[12]["name"]
+

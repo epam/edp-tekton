0d60a9484cb622f42a2b158b687d4c16005b7d82
Artem Zahumonnyi <Artem_Zahumonnyi@epam.com>
2022-10-25 22:13:01 +0300

    [EPMDEDP-10605]: chore: Add Build and Review pipelines of Go for GitHub vcs
    Change-Id: I79daa4ecae1ea9853198973242fbe70180665364

--- charts/pipelines-library/tests/test_go_pipelines.py
+++ charts/pipelines-library/tests/test_go_pipelines.py
@@ -80,3 +80,74 @@ gerrit:
             assert "kaniko-build" in btedp[10]["name"]
             assert "git-tag" in btedp[11]["name"]
             assert "update-cbis" in btedp[12]["name"]
+
+def test_go_pipelines_github():
+    config = """
+github:
+  enabled: true
+    """
+
+    r = helm_template(config)
+    vcs = "github"
+
+    # ensure pipelines have proper steps
+    for buildtool in ['go']:
+        for framework in ['beego']:
+
+            github_review_pipeline = f"{vcs}-{buildtool}-{framework}-app-review"
+            github_build_pipeline_def = f"{vcs}-{buildtool}-{framework}-app-build-default"
+            github_build_pipeline_edp = f"{vcs}-{buildtool}-{framework}-app-build-edp"
+
+            assert github_review_pipeline in r["pipeline"]
+            assert github_build_pipeline_def in r["pipeline"]
+            assert github_build_pipeline_edp in r["pipeline"]
+
+            rt = r["pipeline"][github_review_pipeline]["spec"]["tasks"]
+            assert "github-set-pending-status" in rt[0]["name"]
+            assert "fetch-repository" in rt[1]["name"]
+            assert "init-values" in rt[2]["name"]
+            assert "build" in rt[3]["name"]
+            assert "test" in rt[4]["name"]
+            assert "sonar" in rt[5]["name"]
+            assert "dockerfile-lint" in rt[6]["name"]
+            assert "dockerbuild-verify" in rt[7]["name"]
+            assert "helm-lint" in rt[8]["name"]
+            assert "github-set-success-status" in r["pipeline"][github_review_pipeline]["spec"]["finally"][0]["name"]
+            assert "github-set-failure-status" in r["pipeline"][github_review_pipeline]["spec"]["finally"][1]["name"]
+
+            # build with default versioning
+            btd = r["pipeline"][github_build_pipeline_def]["spec"]["tasks"]
+            assert "fetch-repository" in btd[0]["name"]
+            assert "init-values" in btd[1]["name"]
+            assert "get-version" in btd[2]["name"]
+            # ensure we have default versioning
+            assert f"get-version-{buildtool}-default" == btd[2]["taskRef"]["name"]
+            assert "sast" in btd[3]["name"]
+            assert "test" in btd[4]["name"]
+            assert "golang-build" == btd[4]["taskRef"]["name"]
+            assert "sonar" in btd[5]["name"]
+            assert "sonarqube-scanner" == btd[5]["taskRef"]["name"]
+            assert "build" in btd[6]["name"]
+            assert "golang-build" == btd[6]["taskRef"]["name"]
+            assert "create-ecr-repository" in btd[7]["name"]
+            assert "kaniko-build" in btd[8]["name"]
+            assert "git-tag" in btd[9]["name"]
+            assert "update-cbis" in btd[10]["name"]
+
+            # build with edp versioning
+            btedp = r["pipeline"][github_build_pipeline_edp]["spec"]["tasks"]
+            assert "fetch-repository" in btedp[0]["name"]
+            assert "init-values" in btedp[1]["name"]
+            assert "get-version" in btedp[2]["name"]
+            assert "get-version-edp" == btedp[2]["taskRef"]["name"]
+            assert "sast" in btedp[3]["name"]
+            assert "test" in btedp[4]["name"]
+            assert "golang-build" == btedp[4]["taskRef"]["name"]
+            assert "sonar" in btedp[5]["name"]
+            assert "sonarqube-scanner" == btedp[5]["taskRef"]["name"]
+            assert "build" in btedp[6]["name"]
+            assert "golang-build" == btedp[6]["taskRef"]["name"]
+            assert "create-ecr-repository" in btd[7]["name"]
+            assert "kaniko-build" in btedp[8]["name"]
+            assert "git-tag" in btedp[9]["name"]
+            assert "update-cbis" in btedp[10]["name"]

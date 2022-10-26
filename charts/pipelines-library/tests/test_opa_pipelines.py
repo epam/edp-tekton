0af446c65930b4035d6d8518c0d7223817249af7
Artem Zahumonnyi <Artem_Zahumonnyi@epam.com>
2022-10-26 14:53:00 +0300

    [EPMDEDP-10605]: chore: Add Build and Review pipelines of OPA for GitHub vcs
    Change-Id: I103d17a3244e528afe7c97ccb01a38cad87c3055

--- charts/pipelines-library/tests/test_opa_pipelines.py
+++ charts/pipelines-library/tests/test_opa_pipelines.py
@@ -56,3 +56,55 @@ gerrit:
                 assert "test" in btedp[4]["name"]
                 assert buildtool == btedp[4]["taskRef"]["name"]
                 assert "git-tag" in btedp[5]["name"]
+
+def test_opa_pipelines_github():
+    config = """
+github:
+  enabled: true
+    """
+
+    r = helm_template(config)
+    vcs = "github"
+
+    # ensure pipelines have proper steps
+    for buildtool in ['opa']:
+        for framework in ['opa']:
+            for cbtype in ['lib']:
+
+                github_review_pipeline = f"{vcs}-{buildtool}-{framework}-{cbtype}-review"
+                github_build_pipeline_def = f"{vcs}-{buildtool}-{framework}-{cbtype}-build-default"
+                github_build_pipeline_edp = f"{vcs}-{buildtool}-{framework}-{cbtype}-build-edp"
+
+                assert github_review_pipeline in r["pipeline"]
+                assert github_build_pipeline_def in r["pipeline"]
+                assert github_build_pipeline_edp in r["pipeline"]
+
+
+                rt = r["pipeline"][github_review_pipeline]["spec"]["tasks"]
+                assert "github-set-pending-status" in rt[0]["name"]
+                assert "fetch-repository" in rt[1]["name"]
+                assert "test" in rt[2]["name"]
+
+                assert "github-set-success-status" in r["pipeline"][github_review_pipeline]["spec"]["finally"][0]["name"]
+                assert "github-set-failure-status" in r["pipeline"][github_review_pipeline]["spec"]["finally"][1]["name"]
+
+                # build with default versioning
+                btd = r["pipeline"][github_build_pipeline_def]["spec"]["tasks"]
+                assert "fetch-repository" in btd[0]["name"]
+                assert "init-values" in btd[1]["name"]
+                assert "get-version" in btd[2]["name"]
+                # ensure we have default versioning
+                assert f"get-version-{buildtool}-default" == btd[2]["taskRef"]["name"]
+                assert "test" in btd[3]["name"]
+                assert buildtool == btd[3]["taskRef"]["name"]
+                assert "git-tag" in btd[4]["name"]
+
+                # build with edp versioning
+                btedp = r["pipeline"][github_build_pipeline_edp]["spec"]["tasks"]
+                assert "fetch-repository" in btedp[0]["name"]
+                assert "init-values" in btedp[1]["name"]
+                assert "get-version" in btedp[2]["name"]
+                assert "get-version-edp" == btedp[2]["taskRef"]["name"]
+                assert "test" in btedp[3]["name"]
+                assert buildtool == btedp[3]["taskRef"]["name"]
+                assert "git-tag" in btedp[4]["name"]

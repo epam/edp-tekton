aefc9e0a2ad39778dea86c13c86c6899281e235f
Artem Zahumonnyi <Artem_Zahumonnyi@epam.com>
2022-10-26 18:58:41 +0300

    [EPMDEDP-10605]: chore: Add Build and Review pipelines of Groovy for GitHub vcs
    Change-Id: I052ed6d0a9e5c823cc47bfa13f6b59e83e87c06a

--- charts/pipelines-library/tests/test_groovy_pipelines.py
+++ charts/pipelines-library/tests/test_groovy_pipelines.py
@@ -70,3 +70,65 @@ gerrit:
             assert "codenarc" == btedp[7]["taskRef"]["name"]
             assert "git-tag" in btedp[8]["name"]
             assert "update-cbis" in btedp[9]["name"]
+
+def test_groovy_pipelines_github():
+    config = """
+github:
+  enabled: true
+    """
+
+    r = helm_template(config)
+    vcs = "github"
+
+    # ensure pipelines have proper steps
+    for buildtool in ['codenarc']:
+        for framework in ['codenarc']:
+
+            github_review_pipeline = f"{vcs}-{buildtool}-{framework}-lib-review"
+            github_build_pipeline_def = f"{vcs}-{buildtool}-{framework}-lib-build-default"
+            github_build_pipeline_edp = f"{vcs}-{buildtool}-{framework}-lib-build-edp"
+
+            assert github_review_pipeline in r["pipeline"]
+            assert github_build_pipeline_def in r["pipeline"]
+            assert github_build_pipeline_edp in r["pipeline"]
+
+            rt = r["pipeline"][github_review_pipeline]["spec"]["tasks"]
+            assert "github-set-pending-status" in rt[0]["name"]
+            assert "fetch-repository" in rt[1]["name"]
+            assert "init-values" in rt[2]["name"]
+            assert "sonar" in rt[3]["name"]
+            assert "gradle" == rt[3]["taskRef"]["name"]
+            assert "build" in rt[4]["name"]
+            assert "codenarc" == rt[4]["taskRef"]["name"]
+
+            assert "github-set-success-status" in r["pipeline"][github_review_pipeline]["spec"]["finally"][0]["name"]
+            assert "github-set-failure-status" in r["pipeline"][github_review_pipeline]["spec"]["finally"][1]["name"]
+
+            # build with default versioning
+            btd = r["pipeline"][github_build_pipeline_def]["spec"]["tasks"]
+            assert "fetch-repository" in btd[0]["name"]
+            assert "init-values" in btd[1]["name"]
+            assert "get-version" in btd[2]["name"]
+            # ensure we have default versioning
+            assert "get-version-gradle-default" == btd[2]["taskRef"]["name"]
+            assert "sonar" in btd[3]["name"]
+            assert "gradle" == btd[3]["taskRef"]["name"]
+            assert "build" in btd[4]["name"]
+            assert "codenarc" == btd[4]["taskRef"]["name"]
+            assert "git-tag" in btd[5]["name"]
+            assert "update-cbis" in btd[6]["name"]
+
+            # build with edp versioning
+            btedp = r["pipeline"][github_build_pipeline_edp]["spec"]["tasks"]
+            assert "fetch-repository" in btedp[0]["name"]
+            assert "init-values" in btedp[1]["name"]
+            assert "get-version" in btedp[2]["name"]
+            assert "get-version-edp" == btedp[2]["taskRef"]["name"]
+            assert "update-build-number" in btedp[3]["taskRef"]["name"]
+            assert "update-build-number-gradle" == btedp[3]["taskRef"]["name"]
+            assert "sonar" in btedp[4]["name"]
+            assert "gradle" == btedp[4]["taskRef"]["name"]
+            assert "build" in btedp[5]["name"]
+            assert "codenarc" == btedp[5]["taskRef"]["name"]
+            assert "git-tag" in btedp[6]["name"]
+            assert "update-cbis" in btedp[7]["name"]

f0487230205b7a4ed7ae60a00f818be702dadff4
Yevhenii Pokhvalii <yevhenii_pokhvalii@epam.com>
2022-10-28 21:22:45 +0300

    [EPMDEDP-10606]: chore: Add Gitlab Gradle Java Apps and Libs
    Change-Id: I037eadee53ea91fcddb6d982d2c1cca35321c7d6

--- charts/pipelines-library/tests/test_java_gradle_pipelines.py
+++ charts/pipelines-library/tests/test_java_gradle_pipelines.py
@@ -247,3 +247,108 @@ github:
                     assert "push" in btedp[9]["name"]
                     assert buildtool == btedp[9]["taskRef"]["name"]
                     assert "git-tag" in btedp[10]["name"]
+
+def test_java_pipelines_gitlab():
+    config = """
+gitlab:
+  enabled: true
+    """
+    r = helm_template(config)
+    # ensure pipelines have proper steps
+    for buildtool in ['gradle']:
+        for framework in ['java8', 'java11']:
+            for cbtype in ['app', 'lib']:
+                gitlab_review_pipeline = f"gitlab-{buildtool}-{framework}-{cbtype}-review"
+                gitlab_build_pipeline_def = f"gitlab-{buildtool}-{framework}-{cbtype}-build-default"
+                gitlab_build_pipeline_edp = f"gitlab-{buildtool}-{framework}-{cbtype}-build-edp"
+                assert gitlab_review_pipeline in r["pipeline"]
+                assert gitlab_build_pipeline_def in r["pipeline"]
+                assert gitlab_build_pipeline_edp in r["pipeline"]
+                rt = r["pipeline"][gitlab_review_pipeline]["spec"]["tasks"]
+                assert "report-pipeline-start-to-gitlab" in rt[0]["name"]
+                assert "fetch-repository" in rt[1]["name"]
+                assert "init-values" in rt[2]["name"]
+                assert "compile" in rt[3]["name"]
+                assert "test" in rt[4]["name"]
+                assert "sonar" in rt[5]["name"]
+                if cbtype == "app":
+                    assert "build" in rt[6]["name"]
+                    assert "dockerfile-lint" in rt[7]["name"]
+                    assert "dockerbuild-verify" in rt[8]["name"]
+                    assert "helm-lint" in rt[9]["name"]
+                assert "gitlab-set-success-status" in r["pipeline"][gitlab_review_pipeline]["spec"]["finally"][0]["name"]
+                assert "gitlab-set-failure-status" in r["pipeline"][gitlab_review_pipeline]["spec"]["finally"][1]["name"]
+                # build with default versioning
+                btd = r["pipeline"][gitlab_build_pipeline_def]["spec"]["tasks"]
+                assert "fetch-repository" in btd[0]["name"]
+                assert "init-values" in btd[1]["name"]
+                assert "get-version" in btd[2]["name"]
+                # ensure we have default versioning
+                assert f"get-version-{buildtool}-default" == btd[2]["taskRef"]["name"]
+                if cbtype == "app":
+                    assert "sast" in btd[3]["name"]
+                    assert "compile" in btd[4]["name"]
+                    assert buildtool == btd[4]["taskRef"]["name"]
+                    assert "test" in btd[5]["name"]
+                    assert buildtool == btd[5]["taskRef"]["name"]
+                    assert "sonar" in btd[6]["name"]
+                    assert buildtool == btd[6]["taskRef"]["name"]
+                    assert "build" in btd[7]["name"]
+                    assert buildtool == btd[7]["taskRef"]["name"]
+                    assert "get-nexus-repository-url" in btd[8]["name"]
+                    assert "push" in btd[9]["name"]
+                    assert buildtool == btd[9]["taskRef"]["name"]
+                    assert "create-ecr-repository" in btd[10]["name"]
+                    assert "kaniko-build" in btd[11]["name"]
+                    assert "git-tag" in btd[12]["name"]
+                    assert "update-cbis" in btd[13]["name"]
+                if cbtype == "lib":
+                    assert "compile" in btd[3]["name"]
+                    assert buildtool == btd[3]["taskRef"]["name"]
+                    assert "test" in btd[4]["name"]
+                    assert buildtool == btd[4]["taskRef"]["name"]
+                    assert "sonar" in btd[5]["name"]
+                    assert buildtool == btd[5]["taskRef"]["name"]
+                    assert "build" in btd[6]["name"]
+                    assert buildtool == btd[6]["taskRef"]["name"]
+                    assert "get-nexus-repository-url" in btd[7]["name"]
+                    assert "push" in btd[8]["name"]
+                    assert buildtool == btd[8]["taskRef"]["name"]
+                    assert "git-tag" in btd[9]["name"]
+                # build with edp versioning
+                btedp = r["pipeline"][gitlab_build_pipeline_edp]["spec"]["tasks"]
+                assert "fetch-repository" in btedp[0]["name"]
+                assert "init-values" in btedp[1]["name"]
+                assert "get-version" in btedp[2]["name"]
+                assert "get-version-edp" == btedp[2]["taskRef"]["name"]
+                assert "update-build-number" in btedp[3]["name"]
+                if cbtype == "app":
+                    assert "sast" in btedp[4]["name"]
+                    assert "compile" in btedp[5]["name"]
+                    assert buildtool == btedp[5]["taskRef"]["name"]
+                    assert "test" in btedp[6]["name"]
+                    assert buildtool == btedp[6]["taskRef"]["name"]
+                    assert "sonar" in btedp[7]["name"]
+                    assert buildtool == btedp[7]["taskRef"]["name"]
+                    assert "build" in btedp[8]["name"]
+                    assert buildtool == btedp[8]["taskRef"]["name"]
+                    assert "get-nexus-repository-url" in btedp[9]["name"]
+                    assert "push" in btedp[10]["name"]
+                    assert buildtool == btedp[10]["taskRef"]["name"]
+                    assert "create-ecr-repository" in btedp[11]["name"]
+                    assert "kaniko-build" in btedp[12]["name"]
+                    assert "git-tag" in btedp[13]["name"]
+                    assert "update-cbis" in btedp[14]["name"]
+                if cbtype == "lib":
+                    assert "compile" in btedp[4]["name"]
+                    assert buildtool == btedp[4]["taskRef"]["name"]
+                    assert "test" in btedp[5]["name"]
+                    assert buildtool == btedp[5]["taskRef"]["name"]
+                    assert "sonar" in btedp[6]["name"]
+                    assert buildtool == btedp[6]["taskRef"]["name"]
+                    assert "build" in btedp[7]["name"]
+                    assert buildtool == btedp[7]["taskRef"]["name"]
+                    assert "get-nexus-repository-url" in btedp[8]["name"]
+                    assert "push" in btedp[9]["name"]
+                    assert buildtool == btedp[9]["taskRef"]["name"]
+                    assert "git-tag" in btedp[10]["name"]

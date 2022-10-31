8a0518816c7f494864fa7916bd021d60e90c0831
Yevhenii Pokhvalii <yevhenii_pokhvalii@epam.com>
2022-10-31 22:56:56 +0200

    [EPMDEDP-10606]: chore: Add Gitlab OPA Lib
    Change-Id: I28f7f73fe24549607da5ae6332aac00eacf3aa2c

--- charts/pipelines-library/tests/test_opa_pipelines.py
+++ charts/pipelines-library/tests/test_opa_pipelines.py
@@ -108,3 +108,55 @@ github:
                 assert "test" in btedp[3]["name"]
                 assert buildtool == btedp[3]["taskRef"]["name"]
                 assert "git-tag" in btedp[4]["name"]
+
+def test_opa_pipelines_gitlab():
+    config = """
+gitlab:
+  enabled: true
+    """
+
+    r = helm_template(config)
+    vcs = "gitlab"
+
+    # ensure pipelines have proper steps
+    for buildtool in ['opa']:
+        for framework in ['opa']:
+            for cbtype in ['lib']:
+
+                gitlab_review_pipeline = f"{vcs}-{buildtool}-{framework}-{cbtype}-review"
+                gitlab_build_pipeline_def = f"{vcs}-{buildtool}-{framework}-{cbtype}-build-default"
+                gitlab_build_pipeline_edp = f"{vcs}-{buildtool}-{framework}-{cbtype}-build-edp"
+
+                assert gitlab_review_pipeline in r["pipeline"]
+                assert gitlab_build_pipeline_def in r["pipeline"]
+                assert gitlab_build_pipeline_edp in r["pipeline"]
+
+
+                rt = r["pipeline"][gitlab_review_pipeline]["spec"]["tasks"]
+                assert "report-pipeline-start-to-gitlab" in rt[0]["name"]
+                assert "fetch-repository" in rt[1]["name"]
+                assert "test" in rt[2]["name"]
+
+                assert "gitlab-set-success-status" in r["pipeline"][gitlab_review_pipeline]["spec"]["finally"][0]["name"]
+                assert "gitlab-set-failure-status" in r["pipeline"][gitlab_review_pipeline]["spec"]["finally"][1]["name"]
+
+                # build with default versioning
+                btd = r["pipeline"][gitlab_build_pipeline_def]["spec"]["tasks"]
+                assert "fetch-repository" in btd[0]["name"]
+                assert "init-values" in btd[1]["name"]
+                assert "get-version" in btd[2]["name"]
+                # ensure we have default versioning
+                assert f"get-version-{buildtool}-default" == btd[2]["taskRef"]["name"]
+                assert "test" in btd[3]["name"]
+                assert buildtool == btd[3]["taskRef"]["name"]
+                assert "git-tag" in btd[4]["name"]
+
+                # build with edp versioning
+                btedp = r["pipeline"][gitlab_build_pipeline_edp]["spec"]["tasks"]
+                assert "fetch-repository" in btedp[0]["name"]
+                assert "init-values" in btedp[1]["name"]
+                assert "get-version" in btedp[2]["name"]
+                assert "get-version-edp" == btedp[2]["taskRef"]["name"]
+                assert "test" in btedp[3]["name"]
+                assert buildtool == btedp[3]["taskRef"]["name"]
+                assert "git-tag" in btedp[4]["name"]

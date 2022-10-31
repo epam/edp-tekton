ea8b3ec951e7f19aa6597a76b44aeefca86636b3
Yevhenii Pokhvalii <yevhenii_pokhvalii@epam.com>
2022-10-31 23:24:20 +0200

    [EPMDEDP-10606]: chore: Add Gitlab Terraform Lib
    Change-Id: I76becd4aa2a5a23309c48588278d01c93006e5bc

--- charts/pipelines-library/tests/test_terraform_pipelines.py
+++ charts/pipelines-library/tests/test_terraform_pipelines.py
@@ -118,3 +118,60 @@ github:
                 assert "terraform-lint" in btedp[4]["name"]
                 assert buildtool == btedp[4]["taskRef"]["name"]
                 assert "git-tag" in btedp[5]["name"]
+
+def test_terraform_pipelines_gitlab():
+    config = """
+gitlab:
+  enabled: true
+    """
+
+    r = helm_template(config)
+    vcs = "gitlab"
+
+    # ensure pipelines have proper steps
+    for buildtool in ['terraform']:
+        for framework in ['terraform']:
+            for cbtype in ['lib']:
+
+                gitlab_review_pipeline = f"{vcs}-{buildtool}-{framework}-{cbtype}-review"
+                gitlab_build_pipeline_def = f"{vcs}-{buildtool}-{framework}-{cbtype}-build-default"
+                gitlab_build_pipeline_edp = f"{vcs}-{buildtool}-{framework}-{cbtype}-build-edp"
+
+                assert gitlab_review_pipeline in r["pipeline"]
+                assert gitlab_build_pipeline_def in r["pipeline"]
+                assert gitlab_build_pipeline_edp in r["pipeline"]
+
+                rt = r["pipeline"][gitlab_review_pipeline]["spec"]["tasks"]
+                assert "report-pipeline-start-to-gitlab" in rt[0]["name"]
+                assert "fetch-repository" in rt[1]["name"]
+                assert "init-values" in rt[2]["name"]
+                assert "terraform-format" in rt[3]["name"]
+                assert "terraform-lint" in rt[4]["name"]
+
+                assert "gitlab-set-success-status" in r["pipeline"][gitlab_review_pipeline]["spec"]["finally"][0]["name"]
+                assert "gitlab-set-failure-status" in r["pipeline"][gitlab_review_pipeline]["spec"]["finally"][1]["name"]
+
+                # build with default versioning
+                btd = r["pipeline"][gitlab_build_pipeline_def]["spec"]["tasks"]
+                assert "fetch-repository" in btd[0]["name"]
+                assert "init-values" in btd[1]["name"]
+                assert "get-version" in btd[2]["name"]
+                # ensure we have default versioning
+                assert f"get-version-{buildtool}-default" == btd[2]["taskRef"]["name"]
+                assert "terraform-format" in btd[3]["name"]
+                assert buildtool == btd[3]["taskRef"]["name"]
+                assert "terraform-lint" in btd[4]["name"]
+                assert buildtool == btd[4]["taskRef"]["name"]
+                assert "git-tag" in btd[5]["name"]
+
+                # build with edp versioning
+                btedp = r["pipeline"][gitlab_build_pipeline_edp]["spec"]["tasks"]
+                assert "fetch-repository" in btedp[0]["name"]
+                assert "init-values" in btedp[1]["name"]
+                assert "get-version" in btedp[2]["name"]
+                assert "get-version-edp" == btedp[2]["taskRef"]["name"]
+                assert "terraform-format" in btedp[3]["name"]
+                assert buildtool == btedp[3]["taskRef"]["name"]
+                assert "terraform-lint" in btedp[4]["name"]
+                assert buildtool == btedp[4]["taskRef"]["name"]
+                assert "git-tag" in btedp[5]["name"]

4bd73856d671a96c5c9573750f185816c0996556
Yevhenii Pokhvalii <yevhenii_pokhvalii@epam.com>
2022-10-31 23:15:50 +0200

    [EPMDEDP-10606]: chore: Add Gitlab Docker Lib
    Change-Id: I5f079468849ee69286006bd942adcb29ca44d351

--- charts/pipelines-library/tests/test_container_pipelines.py
+++ charts/pipelines-library/tests/test_container_pipelines.py
@@ -117,3 +117,59 @@ github:
             assert "kaniko-build" in btedp[5]["name"]
             assert "git-tag" in btedp[6]["name"]
             assert "update-cbis" in btedp[7]["name"]
+
+def test_container_pipelines_gitlab():
+    config = """
+gitlab:
+  enabled: true
+    """
+
+    r = helm_template(config)
+    vcs = "gitlab"
+
+    # ensure pipelines have proper steps
+    for buildtool in ['kaniko']:
+        for framework in ['docker']:
+
+            gitlab_review_pipeline = f"{vcs}-{buildtool}-{framework}-lib-review"
+            gitlab_build_pipeline_def = f"{vcs}-{buildtool}-{framework}-lib-build-default"
+            gitlab_build_pipeline_edp = f"{vcs}-{buildtool}-{framework}-lib-build-edp"
+
+            assert gitlab_review_pipeline in r["pipeline"]
+            assert gitlab_build_pipeline_def in r["pipeline"]
+            assert gitlab_build_pipeline_edp in r["pipeline"]
+
+            rt = r["pipeline"][gitlab_review_pipeline]["spec"]["tasks"]
+            assert "report-pipeline-start-to-gitlab" in rt[0]["name"]
+            assert "fetch-repository" in rt[1]["name"]
+            assert "dockerfile-lint" in rt[2]["name"]
+            assert "dockerbuild-verify" in rt[3]["name"]
+
+            assert "gitlab-set-success-status" in r["pipeline"][gitlab_review_pipeline]["spec"]["finally"][0]["name"]
+            assert "gitlab-set-failure-status" in r["pipeline"][gitlab_review_pipeline]["spec"]["finally"][1]["name"]
+
+            # build with default versioning
+            btd = r["pipeline"][gitlab_build_pipeline_def]["spec"]["tasks"]
+            assert "fetch-repository" in btd[0]["name"]
+            assert "init-values" in btd[1]["name"]
+            assert "get-version" in btd[2]["name"]
+            assert f"get-version-{buildtool}-default" == btd[2]["taskRef"]["name"]
+            assert "dockerfile-lint" in btd[3]["name"]
+            assert "create-ecr-repository" in btd[4]["name"]
+            # ensure we have default versioning
+            assert "kaniko-build" in btd[5]["name"]
+            assert buildtool == btd[5]["taskRef"]["name"]
+            assert "git-tag" in btd[6]["name"]
+            assert "update-cbis" in btd[7]["name"]
+
+            # build with edp versioning
+            btedp = r["pipeline"][gitlab_build_pipeline_edp]["spec"]["tasks"]
+            assert "fetch-repository" in btedp[0]["name"]
+            assert "init-values" in btedp[1]["name"]
+            assert "get-version" in btedp[2]["name"]
+            assert "get-version-edp" == btedp[2]["taskRef"]["name"]
+            assert "dockerfile-lint" in btedp[3]["name"]
+            assert "create-ecr-repository" in btedp[4]["name"]
+            assert "kaniko-build" in btedp[5]["name"]
+            assert "git-tag" in btedp[6]["name"]
+            assert "update-cbis" in btedp[7]["name"]

55021b8c6128247cf90a52dcc6f9a3c5e3a07d86
Artem Zahumonnyi <Artem_Zahumonnyi@epam.com>
2022-11-07 13:00:18 +0200

    [EPMDEDP-10803]: chore: Split the code-review trigger for Gerrit and GitHub
    * Applications and docker library use 'edp-kaniko' SA for dockerbuild-verify
    * Autotests and libraries, except docker use default tekton SA
    
    Change-Id: Iec4d7574e08ceda5ef84e4e6c64fde9592ee3659

--- charts/pipelines-library/tests/test_github_integration.py
+++ charts/pipelines-library/tests/test_github_integration.py
@@ -38,7 +38,20 @@ github:
     glab = r["eventlistener"]["github-listener"]["spec"]["triggers"][1]["interceptors"][0]["params"][0]["value"]
     gllb = r["eventlistener"]["github-listener"]["spec"]["triggers"][2]["interceptors"][0]["params"][0]["value"]
     glcb = r["eventlistener"]["github-listener"]["spec"]["triggers"][3]["interceptors"][0]["params"][0]["value"]
-    glcr = r["eventlistener"]["github-listener"]["spec"]["triggers"][4]["interceptors"][0]["params"][0]["value"]
+    glaccr = r["eventlistener"]["github-listener"]["spec"]["triggers"][4]["interceptors"][0]["params"][0]["value"]
+    gllatcr = r["eventlistener"]["github-listener"]["spec"]["triggers"][5]["interceptors"][0]["params"][0]["value"]
 
-    assert "secretString" == glatb["secretKey"] == glab["secretKey"] == gllb["secretKey"] == glcb["secretKey"] == glcr["secretKey"]
-    assert "github.com-config" == glatb["secretName"] == glab["secretName"] == gllb["secretName"] == glcb["secretName"] == glcr["secretName"]
+    assert "secretString" \
+           == glatb["secretKey"] \
+           == glab["secretKey"] \
+           == gllb["secretKey"] \
+           == glcb["secretKey"] \
+           == glaccr["secretKey"] \
+           == gllatcr["secretKey"]
+    assert "github.com-config" \
+           == glatb["secretName"] \
+           == glab["secretName"] \
+           == gllb["secretName"] \
+           == glcb["secretName"] \
+           == glaccr["secretName"] \
+           == gllatcr["secretName"]

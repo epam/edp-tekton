d0762a20dfda30f49c6b56f33ce458694fc6b3ef
Zorian Motso <zorian_motso@epam.com>
2022-12-13 19:08:28 +0200

    [EPMDEDP-11190]: feat: Return empty string when JiraServer is not defined
    Change-Id: I82bfdd4483a26f2eabc030d05622b71de5c44842

--- pkg/interceptor/edp_interceptor.go
+++ pkg/interceptor/edp_interceptor.go
@@ -105,6 +105,10 @@ func (i *EDPInterceptor) Process(ctx context.Context, r *triggersv1.InterceptorR
 		codebase.Spec.CommitMessagePattern = stringP("")
 	}
 
+	if codebase.Spec.JiraServer == nil {
+		codebase.Spec.JiraServer = stringP("")
+	}
+
 	return &triggersv1.InterceptorResponse{
 		Continue: true,
 		Extensions: map[string]interface{}{

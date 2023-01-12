748d58264f321e8b8bff2a21e27a844882221908
Artem Zahumonnyi <Artem_Zahumonnyi@epam.com>
2023-01-12 09:32:09 +0200

    [EPMDEDP-11307]: chore: Add custom pipelines for edp-component-operator
    Change-Id: Iabd38428728524f6a37b2c7e29bdf6b2dc045d80

---
+++ charts/custom-pipelines/tests/test_edp_component_operator.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+from .helpers_custom import helm_template
+
+print(helm_template)
+
+def test_operator():
+    config = """
+global:
+  gitProvider: gerrit
+    """
+
+    ht = helm_template(config)
+    vcs = "gerrit"
+
+    review = f"{vcs}-co-go-other-app-review"
+    build = f"{vcs}-co-go-other-app-build-edp"
+
+    assert review in ht["pipeline"]
+    assert build in ht["pipeline"]
+
+    r = ht["pipeline"][review]["spec"]["tasks"]
+    assert "fetch-repository" in r[0]["name"]
+    assert "gerrit-notify" in r[1]["name"]
+    assert "init-values" in r[2]["name"]
+    assert "commit-validate" in r[3]["name"]
+    assert "helm-docs" in r[4]["name"]
+    assert "helm-lint" in r[5]["name"]
+    assert "crdocs-build" in r[6]["name"]
+    assert "gerrit-vote-success" in ht["pipeline"][review]["spec"]["finally"][0]["name"]
+    assert "gerrit-vote-failure" in ht["pipeline"][review]["spec"]["finally"][1]["name"]
+
+    b = ht["pipeline"][build]["spec"]["tasks"]
+    assert "fetch-repository" in b[0]["name"]
+    assert "gerrit-notify" in b[1]["name"]
+    assert "init-values" in b[2]["name"]
+    assert "get-version" in b[3]["name"]
+    assert "set-version" in b[4]["name"]
+    assert "helm-push-gh-pages" in b[5]["name"]
+    assert "git-tag" in b[6]["name"]
+    assert "update-cbis" in b[7]["name"]
+    assert "update-cbb" in ht["pipeline"][build]["spec"]["finally"][0]["name"]
+    assert "push-to-jira" in ht["pipeline"][build]["spec"]["finally"][1]["name"]

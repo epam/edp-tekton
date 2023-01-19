f8da4976333898ea4dcc64bdc01a7850ac1a9b07
Oleksandr Taruraiev <oleksandr_taruraiev@epam.com>
2023-01-19 13:04:33 +0200

    [EPMDEDP-11291]: test: Unit tests for custom-pipelines
    * Update Makefile separate test for custom-pipelines and pipelines-library
    * Add tests edp-install, headlamp, operator
    
    Change-Id: I6ffa4cb628fa83175ab868f051482811baf90d8f

---
+++ charts/custom-pipelines/tests/helpers_custom.py
@@ -0,0 +1,26 @@
+import tempfile
+import yaml
+import os
+import json
+from subprocess import check_output
+
+
+def kind_builder(helm_chart):
+    results = {}
+    for resource in helm_chart:
+        if resource:
+            kind = resource["kind"].lower()
+            if kind not in results:
+                results[kind] = {}
+            results[kind][resource["metadata"]["name"]] = resource
+    return results
+
+
+def helm_template(config):
+    with tempfile.NamedTemporaryFile() as temp:
+        with open(temp.name, "w") as values:
+            values.write(config)
+        helm_cmd = f"helm template release-name -f {temp.name} ./charts/custom-pipelines --namespace=ns"
+        helm_chart = yaml.load_all(check_output(helm_cmd.split()), Loader=yaml.FullLoader)
+
+        return kind_builder(helm_chart)
---
+++ charts/custom-pipelines/tests/test_headlamp.py
@@ -0,0 +1,56 @@
+import os
+import sys
+
+from .helpers_custom import helm_template
+
+print(helm_template)
+
+def test_operator():
+    config = """
+global:
+  gitProvider: gerrit
+    """
+
+    ht = helm_template(config)
+    vcs = "gerrit"
+
+    review = f"{vcs}-npm-other-app-review"
+    build = f"{vcs}-npm-other-app-build-edp"
+
+    assert review in ht["pipeline"]
+    assert build in ht["pipeline"]
+
+    b = ht["pipeline"][build]["spec"]["tasks"]
+    assert "fetch-repository" in b[0]["name"]
+    assert "gerrit-notify" in b[1]["name"]
+    assert "init-values" in b[2]["name"]
+    assert "get-version" in b[3]["name"]
+    assert "update-build-number" in b[4]["name"]
+    assert "sonar-cleanup" in b[5]["name"]
+    assert "compile" in b[6]["name"]
+    assert "test" in b[7]["name"]
+    assert "sonar" in b[8]["name"]
+    assert "build" in b[9]["name"]
+    assert "get-nexus-repository-url" in b[10]["name"]
+    assert "push" in b[11]["name"]
+    assert "create-ecr-repository" in b[12]["name"]
+    assert "kaniko-build" in b[13]["name"]
+    assert "git-tag" in b[14]["name"]
+    assert "update-cbis" in b[15]["name"]
+
+    r = ht["pipeline"][review]["spec"]["tasks"]
+    assert "fetch-repository" in r[0]["name"]
+    assert "gerrit-notify" in r[1]["name"]
+    assert "init-values" in r[2]["name"]
+    assert "commit-validate" in r[3]["name"]
+    assert "helm-docs" in r[4]["name"]
+    assert "helm-lint" in r[5]["name"]
+    assert "compile" in r[6]["name"]
+    assert "test" in r[7]["name"]
+    assert "fetch-target-branch" in r[8]["name"]
+    assert "sonar-prepare-files" in r[9]["name"]
+    assert "sonar" in r[10]["name"]
+    assert "npm-test" in r[11]["name"]
+    assert "npm-lint" in r[12]["name"]
+    assert "dockerfile-lint" in r[13]["name"]
+    assert "dockerbuild-verify" in r[14]["name"]

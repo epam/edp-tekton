ab5d4208aafb6f654cbfdb8dca2ecba3704430b8
Oleksandr Taruraiev <oleksandr_taruraiev@epam.com>
2023-01-30 15:09:53 +0200

    [EPMDEDP-11291]: test: Unit tests for custom-pipelines
    * Update go build operator
    
    Change-Id: I1438b1812bbd545656d0623e99c1d1d49443f55c

---
+++ charts/custom-pipelines/tests/test_edp_container.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+from .helpers_custom import helm_template
+
+print(helm_template)
+
+def test_operator():
+    config = """
+global:
+  gitProvider: gerrit
+    """
+
+    ht = helm_template(config)
+    vcs = "gerrit"
+
+    review = f"{vcs}-kaniko-other-app-review"
+    build = f"{vcs}-kaniko-other-app-build-edp"
+
+    assert review in ht["pipeline"]
+    assert build in ht["pipeline"]
+
+    b = ht["pipeline"][build]["spec"]["tasks"]
+    assert "fetch-repository" in b[0]["name"]
+    assert "gerrit-notify" in b[1]["name"]
+    assert "init-values" in b[2]["name"]
+    assert "get-version" in b[3]["name"]
+    assert "dockerfile-lint" in b[4]["name"]
+    assert "create-ecr-repository" in b[5]["name"]
+    assert "update-cbb" in ht["pipeline"][build]["spec"]["finally"][0]["name"]
+    assert "push-to-jira" in ht["pipeline"][build]["spec"]["finally"][1]["name"]
+
+    r = ht["pipeline"][review]["spec"]["tasks"]
+    assert "fetch-repository" in r[0]["name"]
+    assert "gerrit-notify" in r[1]["name"]
+    assert "init-values" in r[2]["name"]
+    assert "commit-validate" in r[3]["name"]
+    assert "dockerfile-lint" in r[4]["name"]
+    assert "dockerbuild-verify" in r[5]["name"]
+    assert "gerrit-vote-success" in ht["pipeline"][review]["spec"]["finally"][0]["name"]
+    assert "gerrit-vote-failure" in ht["pipeline"][review]["spec"]["finally"][1]["name"]

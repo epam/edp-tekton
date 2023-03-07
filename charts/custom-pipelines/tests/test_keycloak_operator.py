96d91ff5b7f06da0fd43335885095f2b2ada78cf
Artem Zahumonnyi <Artem_Zahumonnyi@epam.com>
2023-03-07 09:32:03 +0200

    [EPMDEDP-11483]: feat: Add e2e Tekton Task
    e2e Task uses the `vcluster` tool and namespace for testing with `kuttl`
    
    * Added `edp-vcluster-namespace-admin` RoleBinding to access resources in `e2e-vcluster` namespace.
    * Added `e2e-vcluster` namespace.
    * `e2e` Task has four steps:
      1. `vcluster-start` - creating vcluster
      2. `kuttl-test` - running the kuttl tests
      3. `vcluster-remove` - deleting the vcluster
      4. `kuttl-test-verify-exitcode` - checking for exit code of `kuttl-test` step, if 0 then success, if not then error
    
    Change-Id: I33d8839a8b91f039173b23adc611e74c6addbae0

---
+++ charts/custom-pipelines/tests/test_keycloak_operator.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+from .helpers_custom import helm_template
+
+print(helm_template)
+
+def test_keycloak_operator():
+    config = """
+global:
+  gitProvider: gerrit
+    """
+
+    ht = helm_template(config)
+    vcs = "gerrit"
+
+    review = f"{vcs}-go-kc-other-app-review"
+    build = f"{vcs}-go-kc-other-app-build-edp"
+
+    assert review in ht["pipeline"]
+    assert build in ht["pipeline"]
+
+    r = ht["pipeline"][review]["spec"]["tasks"]
+    assert "fetch-repository" in r[0]["name"]
+    assert "gerrit-notify" in r[1]["name"]
+    assert "init-values" in r[2]["name"]
+    assert "commit-validate" in r[3]["name"]
+    assert "helm-docs" in r[4]["name"]
+    assert "helm-lint" in r[5]["name"]
+    assert "crdocs-build" in r[6]["name"]
+    assert "build" in r[7]["name"]
+    assert "test" in r[8]["name"]
+    assert "lint" in r[9]["name"]
+    assert "fetch-target-branch" in r[10]["name"]
+    assert "sonar-prepare-files" in r[11]["name"]
+    assert "sonar" in r[12]["name"]
+    assert "dockerfile-lint" in r[13]["name"]
+    assert "dockerbuild-verify" in r[14]["name"]
+    assert "gerrit-vote-success" in ht["pipeline"][review]["spec"]["finally"][0]["name"]
+    assert "gerrit-vote-failure" in ht["pipeline"][review]["spec"]["finally"][1]["name"]
+
+    b = ht["pipeline"][build]["spec"]["tasks"]
+    assert "fetch-repository" in b[0]["name"]
+    assert "gerrit-notify" in b[1]["name"]
+    assert "init-values" in b[2]["name"]
+    assert "get-version" in b[3]["name"]
+    assert "sonar-cleanup" in b[4]["name"]
+    assert "sast" in b[5]["name"]
+    assert "test" in b[6]["name"]
+    assert "sonar" in b[7]["name"]
+    assert "build" in b[8]["name"]
+    assert "create-ecr-repository" in b[9]["name"]
+    assert "kaniko-build" in b[10]["name"]
+    assert "e2e" in b[11]["name"]
+    assert "ecr-to-docker" in b[12]["name"]
+    assert "set-version" in b[13]["name"]
+    assert "helm-push-gh-pages" in b[14]["name"]
+    assert "git-tag" in b[15]["name"]
+    assert "update-cbis" in b[16]["name"]
+    assert "update-cbb" in ht["pipeline"][build]["spec"]["finally"][0]["name"]
+    assert "push-to-jira" in ht["pipeline"][build]["spec"]["finally"][1]["name"]
+    assert "send-to-microsoft-teams-failed" in ht["pipeline"][build]["spec"]["finally"][2]["name"]

e14ff3a8b7f20958275627181552da9b1458c949
Yevhenii Pokhvalii <yevhenii_pokhvalii@epam.com>
2023-06-07 13:57:00 +0300

    [EPMDEDP-11763]: chore: Change kaniko-build to dockerbuild-verify in custom review pipelines
    Change-Id: Ibd3ab35ff1d72d7a7a90fbbf19cff7c447e2d0c7

--- charts/custom-pipelines/tests/test_edp_container.py
+++ charts/custom-pipelines/tests/test_edp_container.py
@@ -37,7 +37,7 @@ global:
     assert "init-values" in r[2]["name"]
     assert "commit-validate" in r[3]["name"]
     assert "dockerfile-lint" in r[4]["name"]
-    assert "kaniko-build" in r[5]["name"]
+    assert "dockerbuild-verify" in r[5]["name"]
     assert "image-scan" in r[6]["name"]
     assert "gerrit-vote-success" in ht["pipeline"][review]["spec"]["finally"][0]["name"]
     assert "gerrit-vote-failure" in ht["pipeline"][review]["spec"]["finally"][1]["name"]
--- charts/custom-pipelines/tests/test_headlamp.py
+++ charts/custom-pipelines/tests/test_headlamp.py
@@ -35,7 +35,7 @@ global:
     assert "sonar" in r[11]["name"]
     assert "compile" in r[12]["name"]
     assert "dockerfile-lint" in r[13]["name"]
-    assert "kaniko-build" in r[14]["name"]
+    assert "dockerbuild-verify" in r[14]["name"]
     assert "image-scan" in r[15]["name"]
     assert "gerrit-vote-success" in ht["pipeline"][review]["spec"]["finally"][0]["name"]
     assert "gerrit-vote-failure" in ht["pipeline"][review]["spec"]["finally"][1]["name"]

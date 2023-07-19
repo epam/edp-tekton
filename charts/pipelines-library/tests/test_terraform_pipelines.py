2c1ac75fec74310b1ce8013b250b2b0ae1b3d5db
Oleksandr Taruraiev <oleksandr_taruraiev@epam.com>
2023-07-19 19:50:50 +0300

    [EPMDEDP-12300]: feat: Align terraform pre-commit approach for lib
    Change-Id: Idf5ac830f3c1bd850844f10913ccd51e4f66791c

--- charts/pipelines-library/tests/test_terraform_pipelines.py
+++ charts/pipelines-library/tests/test_terraform_pipelines.py
@@ -26,8 +26,7 @@ global:
                 assert "fetch-repository" in rt[0]["name"]
                 assert "gerrit-notify" in rt[1]["name"]
                 assert "init-values" in rt[2]["name"]
-                assert "terraform-format" in rt[3]["name"]
-                assert "terraform-lint" in rt[4]["name"]
+                assert "terraform-check" in rt[3]["name"]
 
                 assert "gerrit-vote-success" in r["pipeline"][gerrit_review_pipeline]["spec"]["finally"][0]["name"]
                 assert "gerrit-vote-failure" in r["pipeline"][gerrit_review_pipeline]["spec"]["finally"][1]["name"]
@@ -40,11 +39,8 @@ global:
                 assert "get-version" in btd[3]["name"]
                 # ensure we have default versioning
                 assert f"get-version-{buildtool}-default" == btd[3]["taskRef"]["name"]
-                assert "terraform-format" in btd[4]["name"]
-                assert buildtool == btd[4]["taskRef"]["name"]
-                assert "terraform-lint" in btd[5]["name"]
-                assert buildtool == btd[5]["taskRef"]["name"]
-                assert "git-tag" in btd[6]["name"]
+                assert "terraform-check" in btd[4]["name"]
+                assert "git-tag" in btd[5]["name"]
                 assert "push-to-jira" in r["pipeline"][gerrit_build_pipeline_def]["spec"]["finally"][0]["name"]
 
                 # build with edp versioning
@@ -54,11 +50,8 @@ global:
                 assert "init-values" in btedp[2]["name"]
                 assert "get-version" in btedp[3]["name"]
                 assert "get-version-edp" == btedp[3]["taskRef"]["name"]
-                assert "terraform-format" in btedp[4]["name"]
-                assert buildtool == btedp[4]["taskRef"]["name"]
-                assert "terraform-lint" in btedp[5]["name"]
-                assert buildtool == btedp[5]["taskRef"]["name"]
-                assert "git-tag" in btedp[6]["name"]
+                assert "terraform-check" in btedp[4]["name"]
+                assert "git-tag" in btedp[5]["name"]
                 assert "update-cbb" in r["pipeline"][gerrit_build_pipeline_edp]["spec"]["finally"][0]["name"]
                 assert "push-to-jira" in r["pipeline"][gerrit_build_pipeline_edp]["spec"]["finally"][1]["name"]
 
@@ -88,8 +81,7 @@ global:
                 assert "github-set-pending-status" in rt[0]["name"]
                 assert "fetch-repository" in rt[1]["name"]
                 assert "init-values" in rt[2]["name"]
-                assert "terraform-format" in rt[3]["name"]
-                assert "terraform-lint" in rt[4]["name"]
+                assert "terraform-check" in rt[3]["name"]
 
                 assert "github-set-success-status" in r["pipeline"][github_review_pipeline]["spec"]["finally"][0]["name"]
                 assert "github-set-failure-status" in r["pipeline"][github_review_pipeline]["spec"]["finally"][1]["name"]
@@ -101,11 +93,8 @@ global:
                 assert "get-version" in btd[2]["name"]
                 # ensure we have default versioning
                 assert f"get-version-{buildtool}-default" == btd[2]["taskRef"]["name"]
-                assert "terraform-format" in btd[3]["name"]
-                assert buildtool == btd[3]["taskRef"]["name"]
-                assert "terraform-lint" in btd[4]["name"]
-                assert buildtool == btd[4]["taskRef"]["name"]
-                assert "git-tag" in btd[5]["name"]
+                assert "terraform-check" in btd[3]["name"]
+                assert "git-tag" in btd[4]["name"]
                 assert "push-to-jira" in r["pipeline"][github_build_pipeline_def]["spec"]["finally"][0]["name"]
 
                 # build with edp versioning
@@ -114,11 +103,8 @@ global:
                 assert "init-values" in btedp[1]["name"]
                 assert "get-version" in btedp[2]["name"]
                 assert "get-version-edp" == btedp[2]["taskRef"]["name"]
-                assert "terraform-format" in btedp[3]["name"]
-                assert buildtool == btedp[3]["taskRef"]["name"]
-                assert "terraform-lint" in btedp[4]["name"]
-                assert buildtool == btedp[4]["taskRef"]["name"]
-                assert "git-tag" in btedp[5]["name"]
+                assert "terraform-check" in btedp[3]["name"]
+                assert "git-tag" in btedp[4]["name"]
                 assert "update-cbb" in r["pipeline"][github_build_pipeline_edp]["spec"]["finally"][0]["name"]
                 assert "push-to-jira" in r["pipeline"][github_build_pipeline_edp]["spec"]["finally"][1]["name"]
 
@@ -148,8 +134,7 @@ global:
                 assert "report-pipeline-start-to-gitlab" in rt[0]["name"]
                 assert "fetch-repository" in rt[1]["name"]
                 assert "init-values" in rt[2]["name"]
-                assert "terraform-format" in rt[3]["name"]
-                assert "terraform-lint" in rt[4]["name"]
+                assert "terraform-check" in rt[3]["name"]
 
                 assert "gitlab-set-success-status" in r["pipeline"][gitlab_review_pipeline]["spec"]["finally"][0]["name"]
                 assert "gitlab-set-failure-status" in r["pipeline"][gitlab_review_pipeline]["spec"]["finally"][1]["name"]
@@ -161,11 +146,8 @@ global:
                 assert "get-version" in btd[2]["name"]
                 # ensure we have default versioning
                 assert f"get-version-{buildtool}-default" == btd[2]["taskRef"]["name"]
-                assert "terraform-format" in btd[3]["name"]
-                assert buildtool == btd[3]["taskRef"]["name"]
-                assert "terraform-lint" in btd[4]["name"]
-                assert buildtool == btd[4]["taskRef"]["name"]
-                assert "git-tag" in btd[5]["name"]
+                assert "terraform-check" in btd[3]["name"]
+                assert "git-tag" in btd[4]["name"]
                 assert "push-to-jira" in r["pipeline"][gitlab_build_pipeline_def]["spec"]["finally"][0]["name"]
 
                 # build with edp versioning
@@ -174,11 +156,8 @@ global:
                 assert "init-values" in btedp[1]["name"]
                 assert "get-version" in btedp[2]["name"]
                 assert "get-version-edp" == btedp[2]["taskRef"]["name"]
-                assert "terraform-format" in btedp[3]["name"]
-                assert buildtool == btedp[3]["taskRef"]["name"]
-                assert "terraform-lint" in btedp[4]["name"]
-                assert buildtool == btedp[4]["taskRef"]["name"]
-                assert "git-tag" in btedp[5]["name"]
+                assert "terraform-check" in btedp[3]["name"]
+                assert "git-tag" in btedp[4]["name"]
                 assert "update-cbb" in r["pipeline"][gitlab_build_pipeline_edp]["spec"]["finally"][0]["name"]
                 assert "push-to-jira" in r["pipeline"][gitlab_build_pipeline_edp]["spec"]["finally"][1]["name"]
 

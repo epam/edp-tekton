e45c61198c7795ccc81c3ee1712da4233ac9cd3c
Sergiy Kulanov <sergiy_kulanov@epam.com>
2023-11-10 14:03:33 +0200

    refactor: Merge fastapi and flask frameworks into single template (#68)
    For EDP we have the same flow for flask and fastapi, so we are merging
    them to avoid code duplication
    
    Signed-off-by: Sergiy Kulanov <sergiy_kulanov@epam.com>
    Change-Id: I0ec82d7fe77537ad5aa4c34288415b4aabb208a5

--- charts/pipelines-library/tests/test_python_pipelines.py
+++ charts/pipelines-library/tests/test_python_pipelines.py
@@ -1,7 +1,8 @@
+import pytest
 from .helpers import helm_template
 
 
-def test_python_pipelines_harbor_gerrit():
+def test_python_common_pipelines_harbor_gerrit():
     config = """
 global:
   gitProvider: gerrit
@@ -105,7 +106,7 @@ global:
                 assert "push-to-jira" in r["pipeline"][gerrit_build_pipeline_edp]["spec"]["finally"][1]["name"]
 
 
-def test_python_pipelines_harbor_github():
+def test_python_common_pipelines_harbor_github():
     config = """
 global:
   gitProvider: github
@@ -199,7 +200,7 @@ global:
                 assert "push-to-jira" in r["pipeline"][github_build_pipeline_edp]["spec"]["finally"][1]["name"]
 
 
-def test_python_pipelines_harbor_gitlab():
+def test_python_common_pipelines_harbor_gitlab():
     config = """
 global:
   gitProvider: gitlab
@@ -293,8 +294,8 @@ global:
                 assert "push-to-jira" in r["pipeline"][gitlab_build_pipeline_edp]["spec"]["finally"][1]["name"]
 
 
-# FastApi
-def test_python_fastapi_pipelines_harbor_gerrit():
+@pytest.mark.parametrize("framework", ["fastapi", "flask"])
+def test_python_pipelines_harbor_gerrit(framework):
     config = """
 global:
   gitProvider: gerrit
@@ -303,7 +304,6 @@ global:
     ht = helm_template(config)
 
     buildtool = "python"
-    framework = "fastapi"
 
     review = f"gerrit-{buildtool}-{framework}-app-review"
     build_default = f"gerrit-{buildtool}-{framework}-app-build-default"
@@ -378,101 +378,8 @@ global:
     assert "push-to-jira" in ht["pipeline"][build_edp]["spec"]["finally"][1]["name"]
 
 
-# Flask
-def test_python_flask_pipelines_harbor_gerrit():
-    config = """
-global:
-  gitProvider: gerrit
-    """
-
-    ht = helm_template(config)
-
-    buildtool = "python"
-    framework = "flask"
-
-    review = f"gerrit-{buildtool}-{framework}-app-review"
-    build_default = f"gerrit-{buildtool}-{framework}-app-build-default"
-    build_edp = f"gerrit-{buildtool}-{framework}-app-build-edp"
-
-    assert review in ht["pipeline"]
-    assert build_default in ht["pipeline"]
-    assert build_edp in ht["pipeline"]
-
-    r = ht["pipeline"][review]["spec"]["tasks"]
-    assert "fetch-repository" in r[0]["name"]
-    assert "gerrit-notify" in r[1]["name"]
-    assert "init-values" in r[2]["name"]
-    assert "helm-docs" in r[3]["name"]
-    assert "test" in r[4]["name"]
-    assert "lint" in r[5]["name"]
-    assert "fetch-target-branch" in r[6]["name"]
-    assert "sonar-prepare-files" in r[7]["name"]
-    assert "sonar-prepare-files-general" == r[7]["taskRef"]["name"]
-    assert "sonar" in r[8]["name"]
-    assert "dockerfile-lint" in r[9]["name"]
-    assert "dockerbuild-verify" in r[10]["name"]
-    assert "helm-lint" in r[11]["name"]
-    assert "gerrit-vote-success" in ht["pipeline"][review]["spec"]["finally"][0]["name"]
-    assert "gerrit-vote-failure" in ht["pipeline"][review]["spec"]["finally"][1]["name"]
-
-    # build with default versioning
-    bd = ht["pipeline"][build_default]["spec"]["tasks"]
-    assert "fetch-repository" in bd[0]["name"]
-    assert "gerrit-notify" in bd[1]["name"]
-    assert "init-values" in bd[2]["name"]
-    assert "get-version" in bd[3]["name"]
-    assert f"get-version-default" == bd[3]["taskRef"]["name"]
-    assert "update-build-number" in bd[4]["name"]
-    assert "sonar-cleanup" in bd[5]["name"]
-    assert "security" in bd[6]["name"]
-    assert "test" in bd[7]["name"]
-    assert buildtool == bd[7]["taskRef"]["name"]
-    assert "lint" in bd[8]["name"]
-    assert buildtool == bd[8]["taskRef"]["name"]
-    assert "compile" in bd[9]["name"]
-    assert buildtool == bd[9]["taskRef"]["name"]
-    assert "sonar" in bd[10]["name"]
-    assert "sonarqube-scanner" == bd[10]["taskRef"]["name"]
-    assert "get-nexus-repository-url" in bd[11]["name"]
-    assert "get-nexus-repository-url" == bd[11]["taskRef"]["name"]
-    assert "push" in bd[12]["name"]
-    assert buildtool == bd[12]["taskRef"]["name"]
-    assert "kaniko-build" in bd[13]["name"]
-    assert "git-tag" in bd[14]["name"]
-    assert "update-cbis" in bd[15]["name"]
-    assert "push-to-jira" in ht["pipeline"][build_default]["spec"]["finally"][0]["name"]
-
-    # build with edp versioning
-    bedp = ht["pipeline"][build_edp]["spec"]["tasks"]
-    assert "fetch-repository" in bedp[0]["name"]
-    assert "gerrit-notify" in bedp[1]["name"]
-    assert "init-values" in bedp[2]["name"]
-    assert "get-version" in bedp[3]["name"]
-    assert "get-version-edp" == bedp[3]["taskRef"]["name"]
-    assert "update-build-number" in bedp[4]["taskRef"]["name"]
-    assert f"update-build-number-{buildtool}" == bedp[4]["taskRef"]["name"]
-    assert "sonar-cleanup" in bedp[5]["name"]
-    assert "security" in bedp[6]["name"]
-    assert "test" in bedp[7]["name"]
-    assert buildtool == bedp[7]["taskRef"]["name"]
-    assert "lint" in bedp[8]["name"]
-    assert buildtool == bedp[8]["taskRef"]["name"]
-    assert "compile" in bedp[9]["name"]
-    assert buildtool == bedp[9]["taskRef"]["name"]
-    assert "sonar" in bedp[10]["name"]
-    assert "sonarqube-scanner" == bedp[10]["taskRef"]["name"]
-    assert "get-nexus-repository-url" in bedp[11]["name"]
-    assert "get-nexus-repository-url" == bedp[11]["taskRef"]["name"]
-    assert "push" in bedp[12]["name"]
-    assert buildtool == bedp[12]["taskRef"]["name"]
-    assert "kaniko-build" in bedp[13]["name"]
-    assert "git-tag" in bedp[14]["name"]
-    assert "update-cbis" in bedp[15]["name"]
-    assert "update-cbb" in ht["pipeline"][build_edp]["spec"]["finally"][0]["name"]
-    assert "push-to-jira" in ht["pipeline"][build_edp]["spec"]["finally"][1]["name"]
-
-
-def test_python_flask_pipelines_harbor_gitlab():
+@pytest.mark.parametrize("framework", ["fastapi", "flask"])
+def test_python_pipelines_harbor_gitlab(framework):
     config = """
 global:
   gitProvider: gitlab
@@ -481,93 +388,6 @@ global:
     ht = helm_template(config)
 
     buildtool = "python"
-    framework = "flask"
-
-    review = f"gitlab-{buildtool}-{framework}-app-review"
-    build_default = f"gitlab-{buildtool}-{framework}-app-build-default"
-    build_edp = f"gitlab-{buildtool}-{framework}-app-build-edp"
-
-    assert review in ht["pipeline"]
-    assert build_default in ht["pipeline"]
-    assert build_edp in ht["pipeline"]
-
-    r = ht["pipeline"][review]["spec"]["tasks"]
-    assert "report-pipeline-start-to-gitlab" in r[0]["name"]
-    assert "fetch-repository" in r[1]["name"]
-    assert "init-values" in r[2]["name"]
-    assert "helm-docs" in r[3]["name"]
-    assert "test" in r[4]["name"]
-    assert "lint" in r[5]["name"]
-    assert "sonar" in r[6]["name"]
-    assert "dockerfile-lint" in r[7]["name"]
-    assert "dockerbuild-verify" in r[8]["name"]
-    assert "helm-lint" in r[9]["name"]
-    assert "gitlab-set-success-status" in ht["pipeline"][review]["spec"]["finally"][0]["name"]
-    assert "gitlab-set-failure-status" in ht["pipeline"][review]["spec"]["finally"][1]["name"]
-
-    # build with default versioning
-    bd = ht["pipeline"][build_default]["spec"]["tasks"]
-    assert "fetch-repository" in bd[0]["name"]
-    assert "init-values" in bd[1]["name"]
-    assert "get-version" in bd[2]["name"]
-    assert f"get-version-default" == bd[2]["taskRef"]["name"]
-    assert "update-build-number" in bd[3]["name"]
-    assert "security" in bd[4]["name"]
-    assert "test" in bd[5]["name"]
-    assert buildtool == bd[5]["taskRef"]["name"]
-    assert "lint" in bd[6]["name"]
-    assert buildtool == bd[6]["taskRef"]["name"]
-    assert "compile" in bd[7]["name"]
-    assert buildtool == bd[7]["taskRef"]["name"]
-    assert "sonar" in bd[8]["name"]
-    assert "sonarqube-scanner" == bd[8]["taskRef"]["name"]
-    assert "get-nexus-repository-url" in bd[9]["name"]
-    assert "get-nexus-repository-url" == bd[9]["taskRef"]["name"]
-    assert "push" in bd[10]["name"]
-    assert buildtool == bd[10]["taskRef"]["name"]
-    assert "kaniko-build" in bd[11]["name"]
-    assert "git-tag" in bd[12]["name"]
-    assert "update-cbis" in bd[13]["name"]
-    assert "push-to-jira" in ht["pipeline"][build_default]["spec"]["finally"][0]["name"]
-
-    # build with edp versioning
-    bedp = ht["pipeline"][build_edp]["spec"]["tasks"]
-    assert "fetch-repository" in bedp[0]["name"]
-    assert "init-values" in bedp[1]["name"]
-    assert "get-version" in bedp[2]["name"]
-    assert "get-version-edp" == bedp[2]["taskRef"]["name"]
-    assert "update-build-number" in bedp[3]["taskRef"]["name"]
-    assert f"update-build-number-{buildtool}" == bedp[3]["taskRef"]["name"]
-    assert "security" in bd[4]["name"]
-    assert "test" in bd[5]["name"]
-    assert buildtool == bd[5]["taskRef"]["name"]
-    assert "lint" in bd[6]["name"]
-    assert buildtool == bd[6]["taskRef"]["name"]
-    assert "compile" in bedp[7]["name"]
-    assert buildtool == bedp[7]["taskRef"]["name"]
-    assert "sonar" in bedp[8]["name"]
-    assert "sonarqube-scanner" == bedp[8]["taskRef"]["name"]
-    assert "get-nexus-repository-url" in bedp[9]["name"]
-    assert "get-nexus-repository-url" == bedp[9]["taskRef"]["name"]
-    assert "push" in bedp[10]["name"]
-    assert buildtool == bedp[10]["taskRef"]["name"]
-    assert "kaniko-build" in bedp[11]["name"]
-    assert "git-tag" in bedp[12]["name"]
-    assert "update-cbis" in bedp[13]["name"]
-    assert "update-cbb" in ht["pipeline"][build_edp]["spec"]["finally"][0]["name"]
-    assert "push-to-jira" in ht["pipeline"][build_edp]["spec"]["finally"][1]["name"]
-
-
-def test_python_fastapi_pipelines_harbor_gitlab():
-    config = """
-global:
-  gitProvider: gitlab
-    """
-
-    ht = helm_template(config)
-
-    buildtool = "python"
-    framework = "fastapi"
 
     review = f"gitlab-{buildtool}-{framework}-app-review"
     build_default = f"gitlab-{buildtool}-{framework}-app-build-default"
@@ -635,7 +455,8 @@ global:
     assert "push-to-jira" in ht["pipeline"][build_edp]["spec"]["finally"][1]["name"]
 
 
-def test_python_flask_pipelines_harbor_github():
+@pytest.mark.parametrize("framework", ["fastapi", "flask"])
+def test_python_pipelines_harbor_github(framework):
     config = """
 global:
   gitProvider: github
@@ -644,93 +465,6 @@ global:
     ht = helm_template(config)
 
     buildtool = "python"
-    framework = "flask"
-
-    review = f"github-{buildtool}-{framework}-app-review"
-    build_default = f"github-{buildtool}-{framework}-app-build-default"
-    build_edp = f"github-{buildtool}-{framework}-app-build-edp"
-
-    assert review in ht["pipeline"]
-    assert build_default in ht["pipeline"]
-    assert build_edp in ht["pipeline"]
-
-    r = ht["pipeline"][review]["spec"]["tasks"]
-    assert "github-set-pending-status" in r[0]["name"]
-    assert "fetch-repository" in r[1]["name"]
-    assert "init-values" in r[2]["name"]
-    assert "helm-docs" in r[3]["name"]
-    assert "test" in r[4]["name"]
-    assert "lint" in r[5]["name"]
-    assert "sonar" in r[6]["name"]
-    assert "dockerfile-lint" in r[7]["name"]
-    assert "dockerbuild-verify" in r[8]["name"]
-    assert "helm-lint" in r[9]["name"]
-    assert "github-set-success-status" in ht["pipeline"][review]["spec"]["finally"][0]["name"]
-    assert "github-set-failure-status" in ht["pipeline"][review]["spec"]["finally"][1]["name"]
-
-    # build with default versioning
-    bd = ht["pipeline"][build_default]["spec"]["tasks"]
-    assert "fetch-repository" in bd[0]["name"]
-    assert "init-values" in bd[1]["name"]
-    assert "get-version" in bd[2]["name"]
-    assert f"get-version-default" == bd[2]["taskRef"]["name"]
-    assert "update-build-number" in bd[3]["name"]
-    assert "security" in bd[4]["name"]
-    assert "test" in bd[5]["name"]
-    assert buildtool == bd[5]["taskRef"]["name"]
-    assert "lint" in bd[6]["name"]
-    assert buildtool == bd[6]["taskRef"]["name"]
-    assert "compile" in bd[7]["name"]
-    assert buildtool == bd[7]["taskRef"]["name"]
-    assert "sonar" in bd[8]["name"]
-    assert "sonarqube-scanner" == bd[8]["taskRef"]["name"]
-    assert "get-nexus-repository-url" in bd[9]["name"]
-    assert "get-nexus-repository-url" == bd[9]["taskRef"]["name"]
-    assert "push" in bd[10]["name"]
-    assert buildtool == bd[10]["taskRef"]["name"]
-    assert "kaniko-build" in bd[11]["name"]
-    assert "git-tag" in bd[12]["name"]
-    assert "update-cbis" in bd[13]["name"]
-    assert "push-to-jira" in ht["pipeline"][build_default]["spec"]["finally"][0]["name"]
-
-    # build with edp versioning
-    bedp = ht["pipeline"][build_edp]["spec"]["tasks"]
-    assert "fetch-repository" in bedp[0]["name"]
-    assert "init-values" in bedp[1]["name"]
-    assert "get-version" in bedp[2]["name"]
-    assert "get-version-edp" == bedp[2]["taskRef"]["name"]
-    assert "update-build-number" in bedp[3]["taskRef"]["name"]
-    assert f"update-build-number-{buildtool}" == bedp[3]["taskRef"]["name"]
-    assert "security" in bedp[4]["name"]
-    assert "test" in bedp[5]["name"]
-    assert buildtool == bedp[5]["taskRef"]["name"]
-    assert "lint" in bedp[6]["name"]
-    assert buildtool == bedp[6]["taskRef"]["name"]
-    assert "compile" in bedp[7]["name"]
-    assert buildtool == bedp[7]["taskRef"]["name"]
-    assert "sonar" in bedp[8]["name"]
-    assert "sonarqube-scanner" == bedp[8]["taskRef"]["name"]
-    assert "get-nexus-repository-url" in bedp[9]["name"]
-    assert "get-nexus-repository-url" == bedp[9]["taskRef"]["name"]
-    assert "push" in bedp[10]["name"]
-    assert buildtool == bedp[10]["taskRef"]["name"]
-    assert "kaniko-build" in bedp[11]["name"]
-    assert "git-tag" in bedp[12]["name"]
-    assert "update-cbis" in bedp[13]["name"]
-    assert "update-cbb" in ht["pipeline"][build_edp]["spec"]["finally"][0]["name"]
-    assert "push-to-jira" in ht["pipeline"][build_edp]["spec"]["finally"][1]["name"]
-
-
-def test_python_fastapi_pipelines_harbor_github():
-    config = """
-global:
-  gitProvider: github
-    """
-
-    ht = helm_template(config)
-
-    buildtool = "python"
-    framework = "fastapi"
 
     review = f"github-{buildtool}-{framework}-app-review"
     build_default = f"github-{buildtool}-{framework}-app-build-default"
